cf3c440d8e55415fd53569ca0beb64890d5fd71e
ysfARR <141406367+ysfARR@users.noreply.github.com>
2023-08-04 11:54:32 +0200

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard_Boiled Free_Range Egg')
