12414d43c9dfebe5a9fb868992816057b917b633
ysfARR <141406367+ysfARR@users.noreply.github.com>
2023-08-07 17:17:30 +0200

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -36,7 +36,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
-      with my_cnx.cursor() as my_cur;
+      with my_cnx.cursor() as my_cur:
            my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
             return my_cur.fetchall()
 # add button to load the fruit
